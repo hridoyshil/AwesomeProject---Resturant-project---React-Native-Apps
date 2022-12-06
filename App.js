15e73d908d0bcd8e2383b0b06c09bcb832b65f09
Hridoy <hriodykumarshil12@gmail.com>
2022-12-06 22:06:47 +0600

    Adding redux

--- App.js
+++ App.js
@@ -4,12 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import AppNavigator from './app/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './app/redux/store';
 
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
